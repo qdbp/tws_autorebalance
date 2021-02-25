b2a23a3d97d9c58c6913bbf237332aab62ceb5e9
Evgeny Naumov <evgeny.naumov@protonmail.com>
2021-02-25 01:56:14 -0500

    infra: add better runner

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
+#!/usr/bin/env python
+import click
+
 from src.app import arb_entrypoint
 
 
-def main() -> None:
-    arb_entrypoint()
+@click.command()
+@click.option("--debug", type=bool, default=False, is_flag=True)
+@click.option("--disarm", type=bool, default=False, is_flag=True)
+def main(debug: bool, disarm: bool) -> None:
+    arb_entrypoint(debug=debug, disarm=disarm)
 
 
 if __name__ == "__main__":
